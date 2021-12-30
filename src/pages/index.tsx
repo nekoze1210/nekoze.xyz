d8d2401efeb5ab966e6749c347b365b3eb877034
Daiki Nagaoka <neko.z.n1210@gmail.com>
2021-12-30 23:27:04 +0900

    :recycle: lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,14 @@ import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-      <div>
-        <Head>
-          <title>Index Page</title>
-        </Head>
-        <main>
-          <h1>Top page</h1>
-        </main>
-      </div>
+    <div>
+      <Head>
+        <title>Index Page</title>
+      </Head>
+      <main>
+        <h1>Top page</h1>
+      </main>
+    </div>
   )
 }
 
