11281e2121921bc055e2beb890a1fff8c91bbe1d
Daiki Nagaoka <neko.z.n1210@gmail.com>
2022-01-16 17:18:30 +0900

    :art: feat: add Type definition about notion API

---
+++ src/types/article.d.ts
@@ -0,0 +1,107 @@
+export type Tag = {
+  name: string
+}
+
+export const BLOCK_TYPES = [
+  'paragraph',
+  'heading_1',
+  'heading_2',
+  'heading_3',
+  'bulleted_list_item',
+  'numbered_list_item',
+  'to_do',
+  'toggle',
+  'child_page',
+  'child_database',
+  'embed',
+  'image',
+  'video',
+  'file',
+  'pdf',
+  'bookmark',
+  'callout',
+  'quote',
+  'equation',
+  'divider',
+  'table_of_contents',
+  'column',
+  'column_list',
+  'link_preview',
+  'synced_block',
+  'template',
+  'link_to_page',
+  'table',
+  'table_row',
+  'code',
+  'unsupported',
+] as const
+export type BlockTuple = typeof BLOCK_TYPES
+export type BlockType = BlockTuple[number]
+
+export type BlockObject = {
+  id: string
+  type: BlockType
+}
+
+export type BlockObjects = (Heading | Divider | Quote | ToDo | List)[]
+
+export type Annotations = {
+  bold: false
+  italic: false
+  strikethrough: false
+  underline: false
+  code: false
+  color: 'default'
+}
+
+export type Text = {
+  content: string | ''
+  annotations: Annotations
+  link: string | null
+}
+
+export type Paragraph = {
+  text: Text[] | []
+} & BlockObject
+
+export type Heading = {
+  text: string | '' // TODO: type alias "Text" に変換できるようにする
+  heading_type: 'heading_1' | 'heading_2' | 'heading_3'
+} & BlockObject
+
+export type Quote = {
+  text: string | '' // TODO: type alias "Text" に変換できるようにする
+} & BlockObject
+
+export type ToDo = {
+  text: string | '' // TODO: type alias "Text" に変換できるようにする
+  isChecked: boolean
+} & BlockObject
+
+export type Code = {
+  text: string
+  language: string
+} & BlockObject
+
+export type Image = {
+  url: string
+  caption: string | '' // TODO: type alias "Text" に変換できるようにする
+} & BlockObject
+
+export type List = {
+  listType: 'bulleted' | 'numbered'
+  text: string | '' // TODO: type alias "Text" に変換できるようにする
+  hasChildren: boolean
+  children: BlockObjects | []
+}
+
+export type Article = {
+  id: string
+  slug: string
+  tags: Tag[]
+  date: Date
+  isPublished: boolean
+  ogImageUrl: string
+  description: string
+  blocks: BlockObjects
+}
