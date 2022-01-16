7940c692db5b6692563b79f16040b7ea68d4976a
Daiki Nagaoka <neko.z.n1210@gmail.com>
2022-01-16 18:03:17 +0900

    :sparkles: feat: add list page index

--- src/lib/notionApi/useCase/index.ts
+++ src/lib/notionApi/useCase/index.ts
@@ -21,6 +21,40 @@ const notion = new Client({
 })
 const databaseId = process.env.NOTION_DATABASE_ID
 
+export const listPublicPages = async () => {
+  const database = await notion.databases.query({
+    database_id: databaseId as string,
+    filter: {
+      and: [
+        {
+          property: 'Published',
+          checkbox: {
+            equals: true,
+          },
+        },
+      ],
+    },
+    sorts: [
+      {
+        property: 'Date',
+        direction: 'descending',
+      },
+    ],
+  })
+  return database.results.map((page: any) => {
+    return {
+      id: page.id,
+      title: page.properties.Page.title[0].plain_text,
+      slug: page.properties.Slug.rich_text[0]?.plain_text,
+      description: page.properties.Description.rich_text[0]?.plain_text || '',
+      ogImageUrl: page.properties.ogImageUrl.rich_text[0]?.plain_text || '',
+      tags: page.properties.Tag.multi_select.map((_) => _.name),
+      date: page.properties.Date.created_time,
+      isPublished: true,
+    }
+  })
+}
+
 export const getPublicPageContentsBySlug = async (slug: string) => {
   // query databases record by Slug property
   const database = await notion.databases.query({
@@ -81,7 +115,6 @@ export const getPublicPageContentsBySlug = async (slug: string) => {
     isPublished: true,
     blocks: articleBlocks,
   }
-  console.log(JSON.stringify(article))
   return article
 }
 
@@ -105,65 +138,74 @@ const getBlocks = async (blockId: string) => {
 }
 
 const toViewModelArticle = (blocksWithChildren: any): BlockObjects => {
-  return blocksWithChildren.map((block: any) => {
-    const articleBlock = {
-      id: block.id,
-      type: block.type as BlockType,
-    }
-    switch (articleBlock.type) {
-      case 'heading_1':
-      case 'heading_2':
-      case 'heading_3':
-        return {
-          ...articleBlock,
-          text: block[block.type].text[0].plain_text,
-          heading_type: block.type,
-        } as Heading
-      case 'paragraph':
-        return {
-          ...articleBlock,
-          text: block.paragraph.text.map((text: TODO) => {
-            return {
-              content: text.plain_text,
-              annotations: { ...text.annotations },
-              link: text.href,
-            } as Text
-          }),
-        } as Paragraph
-      case 'to_do':
-        return {
-          ...articleBlock,
-          isChecked: block.to_do.checked,
-          text: block.to_do.text[0]?.plain_text || '',
-        } as ToDo
-      case 'quote':
-        return {
-          ...articleBlock,
-          text: block.quote.text[0].plain_text,
-        } as Quote
-      case 'code':
-        return {
-          ...articleBlock,
-          text: block.code.text[0].plain_text,
-          language: block.code.language,
-        } as Code
-      case 'bulleted_list_item':
-      case 'numbered_list_item':
-        return {
-          ...articleBlock,
-          text: block[block.type]?.text.map((text: TODO) => text.plain_text).join(''),
-          listType: block.type === 'bulleted_list_item' ? 'bulleted' : 'numbered',
-          hasChildren: block.has_children,
-          children: block.has_children ? toViewModelArticle(block[block.type]?.children) : [],
-        } as List
-      case 'image':
-        return {
-          ...articleBlock,
-          url: block.image.file.url,
-          caption: block.image.caption.map((caption: TODO) => caption.plain_text).join(''),
-        } as Image
-      default:
-        console.warn(`⚠️ unsupported block type: ${block.type}`)
-    }
-  })
+  if (!blocksWithChildren) {
+    return []
+  }
+  return blocksWithChildren
+    .map((block: any) => {
+      const articleBlock = {
+        id: block.id,
+        type: block.type as BlockType,
+      }
+      switch (articleBlock.type) {
+        case 'heading_1':
+        case 'heading_2':
+        case 'heading_3':
+          return {
+            ...articleBlock,
+            text: block[block.type].text[0].plain_text,
+            heading_type: block.type,
+          } as Heading
+        case 'paragraph':
+          return {
+            ...articleBlock,
+            text: block.paragraph.text.map((text: TODO) => {
+              return {
+                content: text.plain_text,
+                annotations: { ...text.annotations },
+                link: text.href,
+              } as Text
+            }),
+          } as Paragraph
+        case 'to_do':
+          return {
+            ...articleBlock,
+            isChecked: block.to_do.checked,
+            text: block.to_do.text[0]?.plain_text || '',
+          } as ToDo
+        case 'quote':
+          return {
+            ...articleBlock,
+            text: block.quote.text[0].plain_text,
+          } as Quote
+        case 'code':
+          return {
+            ...articleBlock,
+            text: block.code.text[0].plain_text,
+            language: block.code.language,
+          } as Code
+        case 'bulleted_list_item':
+        case 'numbered_list_item':
+          return {
+            ...articleBlock,
+            text: block[block.type]?.text.map((text: TODO) => text.plain_text).join(''),
+            listType: block.type === 'bulleted_list_item' ? 'bulleted' : 'numbered',
+            hasChildren: block.has_children,
+            children: block.has_children ? toViewModelArticle(block[block.type]?.children) : [],
+          } as List
+        case 'image':
+          return {
+            ...articleBlock,
+            url: block.image.file.url,
+            caption: block.image.caption.map((caption: TODO) => caption.plain_text).join(''),
+          } as Image
+        default:
+          console.warn(`⚠️ unsupported block type: ${block.type}`)
+      }
+    })
+    .filter(notNull)
+}
+
+const notNull = <T>(item: T | null): item is T => {
+  return item !== null
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,37 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import { listPublicPages } from '@/lib/notionApi/useCase'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }) => {
   return (
     <div>
       <Head>
         <title>Index Page</title>
       </Head>
       <main>
-        <h1>Top page</h1>
+        <ul>
+          {posts.map((post: any) => {
+            return (
+              <Link href={`posts/${encodeURIComponent(post.slug)}`} key={post.id}>
+                <li key={post.id}>{post.title}</li>
+              </Link>
+            )
+          })}
+        </ul>
       </main>
     </div>
   )
 }
 
 export default Home
+
+export const getStaticProps = async () => {
+  const posts = await listPublicPages()
+  console.log(posts)
+  return {
+    props: {
+      posts: posts,
+    },
+  }
+}
