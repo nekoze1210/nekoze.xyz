1a095962aed33546ba99f86ef08e0d2c756d1f30
Daiki Nagaoka <neko.z.n1210@gmail.com>
2022-02-11 22:05:37 +0900

    :sparkles: feat: add tailwind custom properties

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 const MyDocument = () => {
   return (
-    <Html lang='ja-JP' className={'dark dark:bg-gray-700 dark:text-white'}>
+    <Html lang='ja-JP' className={'dark'}>
       <Head>
         <meta name='application-name' content='nekoze.dev' />
       </Head>
-      <body className={'w-full dark:bg-gray-700 dark:text-white'}>
-        <div className={'container mx-auto w-509'}>
+      <body className={'bg-wild-sand text-black dark:bg-cod-gray dark:text-white'}>
+        <div className={'container mx-auto max-w-510px'}>
           <Main />
           <NextScript />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cod-gray': '#151414',
+        'wild-sand': '#F5F5F5',
+      },
+      width: {
+        '510px': '510px',
+      },
+      maxWidth: {
+        '510px': '510px',
+      },
+    },
   },
   variants: {
     extend: {},
