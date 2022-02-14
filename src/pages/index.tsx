2fc9ec93f68ac4c93875286d7784ee73622e467b
Daiki Nagaoka <neko.z.n1210@gmail.com>
2022-02-14 16:04:18 +0900

    :sparkles: feat: add design to posts index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
@@ -13,12 +14,45 @@ const Home: NextPage = ({ posts }) => {
         <title>Index Page</title>
       </Head>
       <main>
-        <ul>
-          {articles.map((article: any) => {
+        <ul className='px-3'>
+          {articles.map((article: any, index: number) => {
             return (
-              <Link href={`posts/${encodeURIComponent(article.slug)}`} key={article.id}>
-                <li key={article.id}>{article.title}</li>
-              </Link>
+              <li
+                className='bg-white rounded-lg border border-aluminium dark:border-abbey dark:bg-black mt-5'
+                key={article.id}
+              >
+                {index < 1 && article.thumbnailImageUrl ? (
+                  <Link href={`/posts/${encodeURIComponent(article.slug)}`} key={article.id}>
+                    <a href='#'>
+                      <img
+                        className='rounded-t-lg object-contain h-48 w-600px bg-wild-sand dark:bg-cod-gray'
+                        src={article.thumbnailImageUrl}
+                        alt=''
+                      />
+                    </a>
+                  </Link>
+                ) : null}
+                <div className='p-5'>
+                  <p className='text-lg'>{dayjs(article.date).format('YYYY-MM-DD')}</p>
+                  <Link href={`/posts/${encodeURIComponent(article.slug)}`} key={article.id}>
+                    <a
+                      key={article.id}
+                      className='mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white'
+                    >
+                      {article.title}
+                    </a>
+                  </Link>
+                  <p>
+                    {article.tags.map((tag: string) => {
+                      return (
+                        <Link href={`/tags/${tag}`} key={article.id + '_' + tag}>
+                          <a className='text-lg'>{`#${tag} `}</a>
+                        </Link>
+                      )
+                    })}
+                  </p>
+                </div>
+              </li>
             )
           })}
         </ul>
