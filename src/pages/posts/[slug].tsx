9eccf78154a5d115a023116346c38515e30c6cfa
Daiki Nagaoka <neko.z.n1210@gmail.com>
2022-02-20 12:27:27 +0900

    :sparkles: feat: add post block components to /posts/slug page

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,6 +1,125 @@
 import { NextPage } from 'next'
 import React from 'react'
+import { CodeBlock } from '@/components/postBlocks/Code'
+import { HeadingBlock } from '@/components/postBlocks/Heading'
+import { ImageBlock } from '@/components/postBlocks/Image'
+import { ListBlock } from '@/components/postBlocks/List'
+import { ParagraphBlock } from '@/components/postBlocks/Paragraph'
+import { QuoteBlock } from '@/components/postBlocks/Quote'
+import { TodoBlock } from '@/components/postBlocks/Todo'
 import { getPublicPageContentsBySlug, listPublicPages } from '@/lib/notionApi/useCase'
+import {
+  Paragraph,
+  BlockObject,
+  Code,
+  Heading,
+  List,
+  Image as PostImage,
+  Quote,
+  ToDo,
+} from '@/types/post'
+
+const renderPostBlock = (block: BlockObject) => {
+  switch (block.type) {
+    case 'paragraph':
+      const paragraph = block as Paragraph
+      return (
+        <div className='mb-3' key={paragraph.id}>
+          <ParagraphBlock
+            id={paragraph.id}
+            type={paragraph.type}
+            texts={paragraph.texts}
+            isChild={paragraph.isChild}
+            key={paragraph.id}
+          />
+        </div>
+      )
+    case 'code':
+      const code = block as Code
+      return (
+        <div className='mb-4' key={code.id}>
+          <CodeBlock
+            text={code.text}
+            language={code.language}
+            id={code.id}
+            type={code.type}
+            isChild={code.isChild}
+          />
+        </div>
+      )
+    case 'heading_1':
+    case 'heading_2':
+    case 'heading_3':
+      const heading = block as Heading
+      return (
+        <div className='mb-4' key={heading.id}>
+          <HeadingBlock
+            id={heading.id}
+            text={heading.text}
+            heading_type={heading.heading_type}
+            type={heading.type}
+            isChild={heading.isChild}
+          />
+        </div>
+      )
+    case 'numbered_list_item':
+    case 'bulleted_list_item':
+      const list = block as List
+      return (
+        <div className='mb-3' key={list.id}>
+          <ListBlock
+            id={list.id}
+            text={list.text}
+            listType={list.listType}
+            childrenBlocks={list.childrenBlocks}
+            hasChildren={list.hasChildren}
+            isChild={list.isChild}
+            type={list.type}
+          >
+            {list.childrenBlocks.map((child: BlockObject) => {
+              if (child.type === 'bulleted_list_item') {
+                return renderPostBlock(child)
+              }
+              return renderPostBlock(child)
+            })}
+          </ListBlock>
+        </div>
+      )
+    case 'image':
+      const image = block as PostImage
+      return (
+        <div className='mb-3' key={image.id}>
+          <ImageBlock
+            id={image.id}
+            type={image.type}
+            url={image.url}
+            isChild={image.isChild}
+            caption={image.caption}
+          />
+        </div>
+      )
+    case 'quote':
+      const quote = block as Quote
+      return (
+        <div className='mb-3' key={quote.id}>
+          <QuoteBlock id={quote.id} text={quote.text} type={quote.type} isChild={quote.isChild} />
+        </div>
+      )
+    case 'to_do':
+      const todo = block as ToDo
+      return (
+        <div className='mb-4' key={todo.id}>
+          <TodoBlock
+            id={todo.id}
+            text={todo.text}
+            isChild={todo.isChild}
+            isChecked={todo.isChecked}
+            type={todo.type}
+          />
+        </div>
+      )
+  }
+}
 
 // @ts-ignore
 const Post: NextPage = ({ post }) => {
@@ -9,28 +128,13 @@ const Post: NextPage = ({ post }) => {
   }
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <pre>
-        <code>{JSON.stringify(post.blocks)}</code>
-        <style jsx>{`
-          code {
-            vertical-align: middle;
-            white-space: pre;
-            word-break: break-all;
-            max-width: 100%;
-            display: block;
-            font-size: 0.8rem;
-            line-height: 1.4;
-            padding: 1.25rem 1.5rem;
-            margin: 0.85rem 0;
-            background-color: #282c34;
-            color: #ccc;
-            border-radius: 6px;
-            overflow: auto;
-          }
-        `}</style>
-      </pre>
+    <div className='bg-white rounded-lg border border-aluminium dark:border-abbey dark:bg-black mt-5 p-5 break-all'>
+      <h1 className='text-2xl'>{post.title}</h1>
+      <article className='mt-3'>
+        {post.blocks.map((block: any) => {
+          return renderPostBlock(block)
+        })}
+      </article>
     </div>
   )
 }
