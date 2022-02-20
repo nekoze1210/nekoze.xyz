645327a06726923ed2125e2aee96c971d1f4e649
Daiki Nagaoka <neko.z.n1210@gmail.com>
2022-02-20 12:25:04 +0900

    :recycle: refactor: update container min width 600 to 806 px

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ const MyDocument = () => {
         <meta name='application-name' content='nekoze.dev' />
       </Head>
       <body className={'bg-wild-sand text-black dark:bg-cod-gray dark:text-white'}>
-        <div className={'container mx-auto max-w-600px'}>
+        <div className={'container mx-auto max-w-806px'}>
           <Main />
           <NextScript />
         </div>
