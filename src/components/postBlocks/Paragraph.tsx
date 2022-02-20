db22d474b502d5c13b2ce0b10764b73668ba5f09
Daiki Nagaoka <neko.z.n1210@gmail.com>
2022-02-20 12:21:17 +0900

    :sparkles: feat: add ParagraphBlock component

---
+++ src/components/postBlocks/Paragraph.tsx
@@ -0,0 +1,18 @@
+import { VFC } from 'react'
+import { TextBlock } from '@/components/postBlocks/Text'
+import { Paragraph } from '@/types/post'
+
+export const ParagraphBlock: VFC<Paragraph> = ({ id, texts }) => {
+  if (!texts) return <p />
+  const textBlocks = texts.map((text, index) => {
+    return (
+      <TextBlock
+        content={text.content}
+        annotations={text.annotations}
+        link={text.link}
+        key={`paragraph_${id}_text_${index}`}
+      />
+    )
+  })
+  return <p className={'mb-4'}>{textBlocks}</p>
+}
