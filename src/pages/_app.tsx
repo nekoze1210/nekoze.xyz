7e2ac1d173ce5600309e31cc496e7ca4466a1984
Daiki Nagaoka <neko.z.n1210@gmail.com>
2022-03-13 14:59:51 +0900

    :mag: feat: added default seo to next layout file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.scss'
+import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 import Layout from '@/pages/layout'
 import 'react-medium-image-zoom/dist/styles.css'
@@ -6,6 +7,19 @@ import 'react-medium-image-zoom/dist/styles.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <DefaultSeo
+        openGraph={{
+          title: '@nekoze_da Blog',
+          description: '思ったことや学んだことを書きます',
+          type: 'website',
+          locale: 'ja_JP',
+          url: process.env.SITE_URL || 'https://blog.nekoze.xyz',
+          site_name: '@nekoze_da Blog',
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+        }}
+      />
       <Component {...pageProps} />
     </Layout>
   )
