a094ffd3e4fe0e6da03f3f8e6fd823e3c114cf81
Daiki Nagaoka <neko.z.n1210@gmail.com>
2022-03-13 10:06:32 +0900

    :sparkles: feat: added zoom component to ImageBlock component

--- src/components/postBlocks/Image.tsx
+++ src/components/postBlocks/Image.tsx
@@ -1,21 +1,16 @@
-import Image from 'next/image'
 import React, { VFC } from 'react'
+import Zoom from 'react-medium-image-zoom'
 import { Image as PostImage } from '@/types/post'
 
 export const ImageBlock: VFC<PostImage> = ({ url, id, caption }) => {
   return (
     <div>
-      <div className='relative'>
-        <Image
-          id={id}
-          src={url}
-          alt={caption.map((text) => text.content).join('') || undefined}
-          width='100%'
-          height='100%'
-          layout='responsive'
-          objectFit='contain'
-        />
-      </div>
+      <figure className={'mx-auto'}>
+        <Zoom overlayBgColorStart='rgba(255, 255, 255, 0)' overlayBgColorEnd='rgba(0, 0, 0, 0.89)'>
+          <img src={url} alt={caption.map((text) => text.content).join('') || undefined} />
+        </Zoom>
+        <figcaption>{caption.map((text) => text.content).join('') || undefined}</figcaption>
+      </figure>
     </div>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from '@/pages/layout'
+import 'react-medium-image-zoom/dist/styles.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
