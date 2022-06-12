9bf5855261bbdf701696dcf35ba4469e8c3c55be
Daiki Nagaoka <neko.z.n1210@gmail.com>
2022-06-12 15:31:01 +0900

    :recycle: refactor: moved BlockType switch conditions to Components/

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import { listPublicPages } from '@/lib/notionApi/useCase'
+import { Post } from '@/types/post'
 
-// @ts-ignore
-const Home: NextPage = ({ posts }) => {
-  const articles = posts || []
+type HomeProps = {
+  posts: Post[]
+}
 
+const Home: NextPage<HomeProps> = ({ posts }) => {
   return (
     <div>
       <Head>
@@ -15,37 +17,37 @@ const Home: NextPage = ({ posts }) => {
       </Head>
       <main>
         <ul className='px-3'>
-          {articles.map((article: any, index: number) => {
+          {posts.map((post, index) => {
             return (
               <li
                 className='bg-white rounded-lg border border-aluminium dark:border-abbey dark:bg-black mt-5'
-                key={article.id}
+                key={post.id}
               >
-                {index < 1 && article.thumbnailImageUrl ? (
-                  <Link href={`/posts/${encodeURIComponent(article.slug)}`} key={article.id}>
+                {index < 1 && post.thumbnailImageUrl ? (
+                  <Link href={`/posts/${encodeURIComponent(post.slug)}`} key={post.id}>
                     <a href='#'>
                       <img
                         className='rounded-t-lg object-contain h-48 w-[806px] bg-wild-sand dark:bg-cod-gray'
-                        src={article.thumbnailImageUrl}
+                        src={post.thumbnailImageUrl}
                         alt=''
                       />
                     </a>
                   </Link>
                 ) : null}
                 <div className='p-5'>
-                  <p className='text-lg'>{dayjs(article.date).format('YYYY-MM-DD')}</p>
-                  <Link href={`/posts/${encodeURIComponent(article.slug)}`} key={article.id}>
+                  <p className='text-lg'>{dayjs(post.date).format('YYYY-MM-DD')}</p>
+                  <Link href={`/posts/${encodeURIComponent(post.slug)}`} key={post.id}>
                     <a
-                      key={article.id}
+                      key={post.id}
                       className='mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white'
                     >
-                      {article.title}
+                      {post.title}
                     </a>
                   </Link>
                   <p>
-                    {article.tags.map((tag: string) => {
+                    {post.tags.map((tag) => {
                       return (
-                        <Link href={`/tags/${tag}`} key={article.id + '_' + tag}>
+                        <Link href={`/tags/${tag}`} key={post.id + '_' + tag}>
                           <a className='text-lg'>{`#${tag} `}</a>
                         </Link>
                       )
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -2,130 +2,15 @@ import dayjs from 'dayjs'
 import { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
 import React from 'react'
-import { CodeBlock } from '@/components/postBlocks/Code'
-import { HeadingBlock } from '@/components/postBlocks/Heading'
-import { ImageBlock } from '@/components/postBlocks/Image'
-import { ListBlock } from '@/components/postBlocks/List'
-import { ParagraphBlock } from '@/components/postBlocks/Paragraph'
-import { QuoteBlock } from '@/components/postBlocks/Quote'
-import { TodoBlock } from '@/components/postBlocks/Todo'
+import { renderPostBlock } from '@/components/PostBlock'
 import { getPublicPageContentsBySlug, listPublicPages } from '@/lib/notionApi/useCase'
-import {
-  Paragraph,
-  BlockObject,
-  Code,
-  Heading,
-  List,
-  Image as PostImage,
-  Quote,
-  ToDo,
-  Tag,
-} from '@/types/post'
+import { Tag, Post } from '@/types/post'
 
-const renderPostBlock = (block: BlockObject) => {
-  switch (block.type) {
-    case 'paragraph':
-      const paragraph = block as Paragraph
-      return (
-        <div className='mb-3' key={paragraph.id}>
-          <ParagraphBlock
-            id={paragraph.id}
-            type={paragraph.type}
-            texts={paragraph.texts}
-            isChild={paragraph.isChild}
-            key={paragraph.id}
-          />
-        </div>
-      )
-    case 'code':
-      const code = block as Code
-      return (
-        <div className='mb-4' key={code.id}>
-          <CodeBlock
-            text={code.text}
-            language={code.language}
-            id={code.id}
-            type={code.type}
-            isChild={code.isChild}
-          />
-        </div>
-      )
-    case 'heading_1':
-    case 'heading_2':
-    case 'heading_3':
-      const heading = block as Heading
-      return (
-        <div className='mb-4' key={heading.id}>
-          <HeadingBlock
-            id={heading.id}
-            texts={heading.texts}
-            heading_type={heading.heading_type}
-            type={heading.type}
-            isChild={heading.isChild}
-          />
-        </div>
-      )
-    case 'numbered_list_item':
-    case 'bulleted_list_item':
-      const list = block as List
-      return (
-        <div className='mb-3' key={list.id}>
-          <ListBlock
-            id={list.id}
-            texts={list.texts}
-            listType={list.listType}
-            childrenBlocks={list.childrenBlocks}
-            hasChildren={list.hasChildren}
-            isChild={list.isChild}
-            type={list.type}
-          >
-            {list.childrenBlocks.map((child: BlockObject) => {
-              if (child.type === 'bulleted_list_item') {
-                return renderPostBlock(child)
-              }
-              return renderPostBlock(child)
-            })}
-          </ListBlock>
-        </div>
-      )
-    case 'image':
-      const image = block as PostImage
-      return (
-        <div className='mb-3' key={image.id}>
-          <ImageBlock
-            id={image.id}
-            type={image.type}
-            url={image.url}
-            isChild={image.isChild}
-            caption={image.caption}
-          />
-        </div>
-      )
-    case 'quote':
-      const quote = block as Quote
-      return (
-        <div className='mb-3' key={quote.id}>
-          <QuoteBlock id={quote.id} texts={quote.texts} type={quote.type} isChild={quote.isChild} />
-        </div>
-      )
-    case 'to_do':
-      const todo = block as ToDo
-      return (
-        <div className='mb-4' key={todo.id}>
-          <TodoBlock
-            id={todo.id}
-            texts={todo.texts}
-            isChild={todo.isChild}
-            isChecked={todo.isChecked}
-            type={todo.type}
-          />
-        </div>
-      )
-  }
+type PostProps = {
+  post: Post
 }
 
-// @ts-ignore
-const Post: NextPage = ({ post }) => {
+const Post: NextPage<PostProps> = ({ post }) => {
   if (!post) {
     return <div>ページが存在しません</div>
   }
@@ -139,7 +24,7 @@ const Post: NextPage = ({ post }) => {
           url: `${process.env.SITE_URL}/${post.slug}`,
           article: {
             authors: ['@nekoze_da'],
-            publishedTime: dayjs(post.publishedAt).format(),
+            publishedTime: dayjs(post.date).format(),
             tags: post.tags.map((tag: Tag) => tag.name),
           },
         }}
