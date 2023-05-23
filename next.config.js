ba97dc0a9a6de6d6517753e431ca9d514cc5c6c9
Daiki Nagaoka <neko.z.n1210@gmail.com>
2023-05-23 10:47:55 +0900

    :wrench: feat: Added redirect settings

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 
+const { sources } = require('next/dist/compiled/webpack/webpack')
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
@@ -8,5 +10,29 @@ module.exports = {
   },
   images: {
     domains: ['s3.us-west-2.amazonaws.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'blog.nekoze.xyz',
+          },
+        ],
+        destination: '/posts/:path*',
+      },
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/posts/:path*',
+        destination: 'https://blog.nekoze.xyz/:path*',
+        permanent: false,
+        basePath: false,
+      },
+    ]
   },
 }
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import React from 'react'
 
+import { PostThumbnail } from '@/components/PostThumbnail'
 import { listPublicPages } from '@/infra/notionApi/client'
 import { Post } from '@/types/post'
 
@@ -9,20 +10,24 @@ type HomeProps = {
   posts: Post[]
 }
 
-const Home: NextPage<HomeProps> = ({ posts }) => {
+const Posts: NextPage<HomeProps> = ({ posts }) => {
   return (
     <div>
       <Head>
-        <title>nekoze.xyz</title>
+        <title>nekoLog</title>
       </Head>
       <main>
-        <p>TODO</p>
+        <ul className='px-3'>
+          {posts.map((post) => {
+            return <PostThumbnail {...post} key={post.id} />
+          })}
+        </ul>
       </main>
     </div>
   )
 }
 
-export default Home
+export default Posts
 
 export const getStaticProps = async () => {
   const posts = await listPublicPages()
