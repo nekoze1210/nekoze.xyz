69e9b9667c9bdee5e4ee012b58fd015bc9518fc4
Daiki Nagaoka <neko.z.n1210@gmail.com>
2023-05-28 20:38:54 +0900

    :recycle: chore: sleep 500ms in getChildrenBlocks

--- src/infra/notionApi/endpoint.ts
+++ src/infra/notionApi/endpoint.ts
@@ -5,6 +5,7 @@ import {
 } from '@notionhq/client/build/src/api-endpoints'
 
 import { NotionBlockObject, NotionDatabaseObject } from '@/infra/notionApi/types'
+import { sleep } from '@/utils/sleep'
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -32,6 +33,7 @@ export const getChildrenBlocks = async (blockId: string, depth = 0) => {
       if ('type' in block) {
         const children = await getChildrenBlocks(block.id, depth + 1)
         blocks.push({ ...block, depth, children })
+        await sleep(500)
       }
     }
     cursor = res.has_more ? res.next_cursor : null
