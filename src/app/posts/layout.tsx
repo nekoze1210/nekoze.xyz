93d07daad53fee6b215951d939dffdaa8a63eaad
Daiki Nagaoka <neko.z.n1210@gmail.com>
2023-06-02 16:37:32 +0900

    :sparkles: feat: updated posts page layout

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -3,25 +3,18 @@ import '@/styles/globals.scss'
 import 'react-medium-image-zoom/dist/styles.css'
 import { DefaultSeo } from 'next-seo'
 
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-
 export default function PostsLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={'container mx-auto max-w-[806px]'}>
-      <div className={'mx-auto my-3 p-3'}>
-        <Header />
-      </div>
+    <div className={'container mx-auto max-w-[1200px]'}>
       <div className={'min-h-screen p-3'}>
         <section>
           <DefaultSeo
             openGraph={{
-              title: 'nekoLog',
-              description: '思ったことや学んだことを書きます',
+              title: 'nekoze.xyz',
               type: 'website',
               locale: 'ja_JP',
-              url: process.env.BLOG_SITE_URL || 'https://blog.nekoze.xyz',
-              site_name: 'nekolog',
+              url: `${process.env.SITE_URL}/posts` || 'https://nekoze.xyz/posts',
+              site_name: 'nekoze.xyz',
             }}
             twitter={{
               cardType: 'summary_large_image',
@@ -29,9 +22,6 @@ export default function PostsLayout({ children }: { children: React.ReactNode })
           />
           {children}
         </section>
-      </div>
-      <div className={'mx-auto my-3'}>
-        <Footer />
       </div>
     </div>
   )
