15cdbec94acb5a57365aa9fcebe51b96d29e0f45
Daiki Nagaoka <neko.z.n1210@gmail.com>
2023-06-02 16:39:18 +0900

    :recycle: chore: added break-word property for small display

--- src/components/portfolio/TileItemText.tsx
+++ src/components/portfolio/TileItemText.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from 'react'
 
 const TileItemText: FC<{ children: ReactNode }> = ({ children }) => {
-  return <p className={'text-[#585858] dark:text-[#DBDBDB]'}>{children}</p>
+  return <p className={'text-[#585858] dark:text-[#DBDBDB] break-words'}>{children}</p>
 }
 
 export default TileItemText
